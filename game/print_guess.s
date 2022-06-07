c974a4360d945a26c3a351bc9b805773b2499473
sustainablelobster <sustainablelobster@protonmail.com>
2022-06-06 21:16:47 -0400

    print_guess working

--- game/print_guess.s
+++ game/print_guess.s
@@ -2,68 +2,112 @@
 .section .text
 .extern strcpy
 .extern strchr
-.extern color_print
+.extern print
 .global print_guess
 .type print_guess, %function
 
 @ void print_guess(const char *guess, const char *answer)
 @   Prints guess to screen with appropriate highlighting.
 print_guess:
-    push    {r4 - r8, lr}
-    mov     r4, r0
+    push    {r4 - r7, lr}       @ char answer_cpy[6];
+    mov     r4, r0              @ char letter_box[4] = "[ ]";
     ldr     r5, =answer_cpy
-    ldr     r6, =letter_box 
-    eor     r7, r7   
+    ldr     r6, =4f
 
-    mov     r0, r1          @ strcpy(answer, answer_cpy)
+    mov     r0, r1              @ strcpy(answer, answer_cpy);
     mov     r1, r5
     bl      strcpy
+    eor     r7, r7
 
-@ loop
+@ loop 0 - mark correct letters
 0:
-    cmp     r7, #5          @ int32_t i = 0;
-    beq     2f              @ int32_t color;
-                            @ char *match;
-    ldrb    r0, [r4, r7]    @ while (i < 5) {
-    cmp     r0, #' '        @   if (guess[i] == ' ') {
-    moveq   r8, #0          @       color = 0;
-    beq     1f              @   } else if (guess[i] == answer_cpy[i]) {
-                            @       answer_cpy[i] = '#';
-    ldrb    r1, [r5, r7]    @       color = 1;
-    cmp     r0, r1          @   } else if (match = strchr(answer_cpy, guess[i])) {
-    moveq   r8, #1          @       *match = '#';
-    moveq   r0, #'#'        @       color = 2;
-    strbeq  r0, [r5, r7]    @   } else {
-    beq     1f              @       color = 3;
-                            @   }
-    mov     r1, r0          @   letter_box[1] = guess[i];
-    ldr     r0, =answer_cpy @   color(letter_box, color);
-    bl      strchr          @   i++;
-    cmp     r0, #0          @ }
-    movne   r8, #2
-    movne   r1, #'#'
-    strbne  r1, [r0]
-    moveq   r8, #3
+    ldrb    r0, [r4, r7]        @ for (int32_t i = 0; i < 5; i++) {
+    ldrb    r1, [r5, r7]        @   if (guess[i] == answer_cpy[i]) {
+    cmp     r0, r1              @       answer_cpy[i] = '=';
+    bne     1f                  @   }
+                                @ }
+    mov     r0, #'='
+    strb    r0, [r5, r7]
 
-@ print letter
+@ prep next loop iteration
 1:
+    add     r7, #1
+    cmp     r7, #5
+    blt     0b
+    eor     r7, r7
+
+@ loop 2 - mark almost correct & incorrect letters, print
+2:
+    ldrb    r0, [r5, r7]        @ for (int32_t i = 0; i < 5; i++) {
+    cmp     r0, #'='            @   const char *color;
+    ldreq   r0, =6f             @   char *strchr_ret = strchr(answer_cpy, guess[i]);
+    beq     3f                  @   if (answer_cpy[i] == '=') {
+                                @       color = 6f; // green (correct);
+    mov     r0, r5              @   } else if (strchr_ret) {
+    ldrb    r1, [r4, r7]        @       color = 7f; // orangish (almost correct)
+    bl      strchr              @       *strchr_ret = '~'; 
+    mov     r1, r0              @   } else {
+    cmp     r0, #0              @       color = 8f; // gray (incorrect)
+    ldreq   r0, =8f             @   }
+    beq     3f                  @   print(color);
+                                @   letter_box[i] = guess[i];
+    mov     r0, #'~'            @   print(letter_box);
+    strb    r0, [r1]            @ }
+    ldr     r0, =7f
+
+@ print and prep next loop iteration
+3:
+    bl      print
     ldrb    r0, [r4, r7]
     strb    r0, [r6, #1]
-
     mov     r0, r6
-    mov     r1, r8
-    bl      color_print
+    bl      print
 
     add     r7, #1
-    b       0b
+    cmp     r7, #5
+    blt     2b
 
-@ return
-2:
-    pop     {r4 - r8, pc}
+    ldr     r0, =5f
+    bl      print
+
+    pop     {r4 - r7, pc}
+
+
+.section .bss
+    .lcomm  answer_cpy, 6
+
 
 .section .data
-letter_box:
+@ letter box
+4:
     .asciz  "[ ]"
 
-answer_cpy:
-    .asciz  "     "
+
+.section .rodata
+@ default colors
+5:
+    .byte   0x1b
+    .ascii  "[39m"
+    .byte   0x1b
+    .asciz  "[49m"
+
+@ green (correct)
+6:
+    .byte   0x1b
+    .ascii  "[97m"
+    .byte   0x1b
+    .asciz  "[42m"
+
+@ yellow/orange  (almost)
+7:
+    .byte   0x1b
+    .ascii  "[97m"
+    .byte   0x1b
+    .asciz  "[43m"
+
+@ gray (incorrect)
+8:
+    .byte   0x1b
+    .ascii  "[97m"
+    .byte   0x1b
+    .asciz  "[100m"
--- testing/harness.c
+++ testing/harness.c
@@ -4,7 +4,7 @@
 extern void print_guess(const char *, const char *);
 
 int main() {
-    print_guess("masts", "makos");
+    print_guess("mosts", "makos");
     puts("");
     return 0;
 }

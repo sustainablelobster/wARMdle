c8eebc3feb551ddda1256ba944288888db6335e7
sustainablelobster <sustainablelobster@protonmail.com>
2022-06-10 15:55:00 -0400

    cleaned trim_newline

--- src/trim_newline.s
+++ src/trim_newline.s
@@ -1,19 +1,19 @@
-.syntax unified
-.section .text
-.global trim_newline
-.type trim_newline, %function
+.syntax     unified
+.global     trim_newline
+.type       trim_newline, %function
 
+.section    .text
 @ void trim_newline(char *str)
 @   Trim trailing newline char from str.
 trim_newline:
     ldrb    r1, [r0]        @ while (*str != 0) {
     cmp     r1, #0          @   if (*str == '\n') {
-    beq     .LTN_return     @       *str = 0;
+    beq     return          @       *str = 0;
     cmp     r1, #'\n'       @       break;
     addne   r0, #1          @   }
     bne     trim_newline    @   str++;
     eor     r1, r1          @ }
     strb    r1, [r0]
 
-.LTN_return:
+return:
     bx      lr              @ return;

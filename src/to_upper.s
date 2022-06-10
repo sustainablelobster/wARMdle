755a5a2dcb460dd2b9ac5d080d517cfff062f81e
sustainablelobster <sustainablelobster@protonmail.com>
2022-06-10 15:48:49 -0400

    cleaned up to_upper

--- src/to_upper.s
+++ src/to_upper.s
@@ -1,24 +1,24 @@
-.syntax unified
-.section .text
-.global to_upper
-.type to_upper, %function
+.syntax     unified
+.global     to_upper
+.type       to_upper, %function
 
+.section    .text
 @ void to_upper(char *str)
 @   Converts all lowercase characters to uppercase.
 to_upper:
     ldrb    r1, [r0]        @ while (*str != 0) {
     cmp     r1, #0          @   if (*str >= 'a' && *str <= 'z') {
-    beq     .LTU_return     @       *str &= 0xdf;
+    beq     return          @       *str &= 0xdf;
     cmp     r1, #'a'        @   }
-    blt     .LTU_next_iter  @   str++;
+    blt     continue        @   str++;
     cmp     r1, #'z'        @ }
-    bgt     .LTU_next_iter
-    and     r1, 0xdf
+    bgt     continue
+    and     r1, #0xdf
     strb    r1, [r0]
 
-.LTU_next_iter:
+continue:
     add     r0, #1
     b       to_upper
 
-.LTU_return:
+return:
     bx      lr              @ return;

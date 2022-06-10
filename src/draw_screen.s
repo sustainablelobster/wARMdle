0d1f932152ed0b67f56188f5e49943f803222bd2
student <student@example.com>
2022-06-10 09:31:30 -0400

    cleaned up draw_screen

--- src/draw_screen.s
+++ src/draw_screen.s
@@ -1,15 +1,40 @@
-.syntax unified
-.section .text
-.extern print
-.extern print_title
-.extern print_guess
-.extern print_empty_row
-.extern clear_screen
-.global draw_screen
-.type draw_screen, %function
+.syntax     unified
+.extern     print
+.extern     print_empty_row
+.extern     print_guess
+.extern     print_title
+.global     draw_screen
+.type       draw_screen, %function
 
+.section    .rodata
+clear_code:
+    .byte   0x1b
+    .ascii  "[1;1H"
+    .byte   0x1b
+    .asciz  "[2J"
+
+title:
+    .ascii  "             _     ___   __  __      _   _       \n"
+    .ascii  " __ __ __   /_\\   | _ \\ |  \\/  |  __| | | |  ___ \n"
+    .ascii  " \\ V  V /  / _ \\  |   / | |\\/| | / _` | | | / -_)\n"
+    .asciz  "  \\_/\\_/  /_/ \\_\\ |_|_\\ |_|  |_| \\__,_| |_| \\___|\n\n"
+
+row_indent:
+    .asciz  "                 "
+
+empty_row:
+    .asciz  "[ ][ ][ ][ ][ ]\n"
+
+
+.section    .text
 @ void draw_screen(int32_t turn, char guesses[6][6], const char *answer)
 @   Prints game screen.
+@
+@   Register usage:
+@       r4 - int32_t turn - current game turn (0-5)
+@       r5 - char guesses[6][6] - player guesses
+@       r6 - char *answer - word player is trying to guess
+@       r7 - int32_t counter - count for print_row_loop
 draw_screen:
     push    {r4 - r7, lr}
     mov     r4, r0
@@ -17,53 +42,33 @@ draw_screen:
     mov     r6, r2
     eor     r7, r7
 
-    ldr     r0, =.LDS_clear     @ print(.LDS_clear);
+    ldr     r0, =clear_code     @ print(clear_code);
     bl      print
 
-    ldr     r0, =.LDS_title     @ print(.LDS_title);
+    ldr     r0, =title          @ print(title);
     bl      print
 
-.LDS_loop:
-    ldr     r0, =.LDS_indent    @ for (int32_t i = 0; i < 6; i++) {
-    bl      print               @   print(.LDS_indent);
+print_row_loop:
+    ldr     r0, =row_indent     @ for (int32_t i = 0; i < 6; i++) {
+    bl      print               @   print(row_indent);
                                 @
     cmp     r4, r7              @   if (i < turn) {
-    ble     .LDS_print_empty    @       print_guess(guess[i], answer);
+    ble     print_empty_row     @       print_guess(guess[i], answer);
                                 @       continue;
     mov     r0, #6              @   }
     mul     r0, r7              @
-    add     r0, r5              @   print(.LDS_empty_row);
+    add     r0, r5              @   print(empty_row);
     mov     r1, r6              @ }
     bl      print_guess
-    b       .LDS_next_iter
+    b       continue
 
-.LDS_print_empty:
-    ldr     r0, =.LDS_empty_row
+print_empty_row:
+    ldr     r0, =empty_row
     bl      print
 
-.LDS_next_iter:
+continue:
     add     r7, #1
     cmp     r7, #6
-    blt     .LDS_loop
+    blt     print_row_loop
 
     pop     {r4 - r7, pc}
-
-
-.section .rodata
-.LDS_clear:
-    .byte   0x1b
-    .ascii  "[1;1H"
-    .byte   0x1b
-    .asciz  "[2J"
-
-.LDS_title:
-    .ascii  "             _     ___   __  __      _   _       \n"
-    .ascii  " __ __ __   /_\\   | _ \\ |  \\/  |  __| | | |  ___ \n"
-    .ascii  " \\ V  V /  / _ \\  |   / | |\\/| | / _` | | | / -_)\n"
-    .asciz  "  \\_/\\_/  /_/ \\_\\ |_|_\\ |_|  |_| \\__,_| |_| \\___|\n\n"
-
-.LDS_indent:
-    .asciz  "                 "
-
-.LDS_empty_row:
-    .asciz  "[ ][ ][ ][ ][ ]\n"

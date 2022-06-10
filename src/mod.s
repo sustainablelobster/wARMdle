8833ca6f79f3bd1525a8eac124990bab32bbf604
student <student@example.com>
2022-06-10 10:28:16 -0400

    cleaned up mod

--- src/mod.s
+++ src/mod.s
@@ -1,15 +1,15 @@
-.syntax unified
-.section .text
-.global mod
-.type mod, %function
+.syntax     unified
+.global     mod
+.type       mod, %function
 
+.section    .text
 @ uint32_t mod(uint32_t dividend, uint32_t divisor)
 @   Returns the remainder of dividend / divisor.
 mod:
-    cmp r0, r1          @ uint32_t remainder = dividend;
-    blo .LMOD_return    @ while (remainder >= divsor)
-    sub r0, r1          @   remainder -= divisor;
-    b   mod
+    cmp     r0, r1          @ uint32_t remainder = dividend;
+    blo     return          @ while (remainder >= divisor)
+    sub     r0, r1          @   remainder -= divisor;
+    b       mod
 
-.LMOD_return:
-    bx  lr              @ return remainder
+return:
+    bx      lr              @ return remainder;

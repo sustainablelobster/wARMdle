7dd887ad4878d0a8b997798a8126e569ac246eb6
student <student@example.com>
2022-06-10 10:45:19 -0400

    cleaned up print_end_msg

--- src/print_end_msg.s
+++ src/print_end_msg.s
@@ -1,40 +1,44 @@
-.syntax unified
-.section .text
-.extern print
-.global print_end_msg
-.type print_end_msg, %function
+.syntax     unified
+.extern     print
+.global     print_end_msg
+.type       print_end_msg, %function
 
+.section .rodata
+win_msg:
+    .asciz  "\nYOU WIN!\n"
+
+lose_msg_open:
+    .asciz  "\nThe word was \""
+
+lose_msg_close:
+    .asciz  "\"\n"
+
+
+.section .text
 @ void print_end_msg(int32_t player_won, const char *answer)
 @   Print message to player informing them that their guess is not on wordlist.
+@
+@   Register usage:
+@       r4 - const char *answer - word player was trying to guess
 print_end_msg:
     push    {r4, lr}
     mov     r4, r1
 
     cmp     r0, #1                  @ if (player_won) {
-    beq     .LPEM_player_won        @   print(.LPEM_win);
+    beq     player_won              @   print(win_msg);
                                     @ } else {
-    ldr     r0, =.LPEM_lose_open    @   print(.LPEM_lose_open);
+    ldr     r0, =lose_msg_open      @   print(lose_msg_open);
     bl      print                   @   print(answer);
-    mov     r0, r4                  @   print(.LPEM_lose_close);
+    mov     r0, r4                  @   print(lose_msg_close);
     bl      print                   @ }
-    ldr     r0, =.LPEM_lose_close
-    bl      print
-    b       .LPEM_return
-
-.LPEM_player_won:
-    ldr     r0, =.LPEM_win
+    ldr     r0, =lose_msg_close
     bl      print
 
-.LPEM_return:
+    b       return
+
+player_won:
+    ldr     r0, =win_msg
+    bl      print
+
+return:
     pop     {r4, pc}
-
-
-.section .rodata
-.LPEM_win:
-    .asciz  "\nYOU WIN!\n"
-
-.LPEM_lose_open:
-    .asciz  "\nThe word was \""
-
-.LPEM_lose_close:
-    .asciz  "\"\n"
